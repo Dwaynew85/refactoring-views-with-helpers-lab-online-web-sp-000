de21c4ef939f79c692ddd241a5b545e7affb0691
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-09-20 06:25:39 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,7 +2,7 @@ module SongsHelper
 
   def display_artist(song)
     if song.artist
-      link_ to song.artist_name, artist_path song.artist
+      link_ to song.artist_name, artist_path(song.artist)
     end
   end
 
